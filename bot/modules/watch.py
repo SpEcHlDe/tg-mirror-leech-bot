c6debea28d2c14283371673af4c8bb5cc4bdd596
r0xtar <72034576+r0xtar@users.noreply.github.com>
2021-11-14 00:45:03 +0530

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -17,11 +17,11 @@ def _watch(bot: Bot, update, isTar=False, isZip=False, isLeech=False):
     try:
         link = message_args[1]
     except IndexError:
-        msg = f"/{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] to mirror with youtube-dl.\n\n"
+        msg = f"/{BotCommands.WatchCommand} [yt-dlp supported link] [quality] |[CustomName] to mirror with yt-dlp.\n\n"
         msg += "<b>Note: Quality and custom name are optional</b>\n\nExample of quality: audio, 144, 240, 360, 480, 720, 1080, 2160."
         msg += "\n\nIf you want to use custom filename, enter it after |"
-        msg += f"\n\nExample:\n<code>/{BotCommands.WatchCommand} https://youtu.be/Pk_TthHfLeE 720 |Slam</code>\n\n"
-        msg += "This file will be downloaded in 720p quality and it's name will be <b>Slam</b>"
+        msg += f"\n\nExample:\n<code>/{BotCommands.WatchCommand} https://youtu.be/kJQP7kiw5Fk 720 |Despacito</code>\n\n"
+        msg += "This file will be downloaded in 720p quality and it's name will be <b>Despacito</b>"
         sendMessage(msg, bot, update)
         return
     
